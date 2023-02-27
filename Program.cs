436a06718681c279ccb0b6d32bc7822252b55789
Jeremy Goodman <jeremygoodman52@gmail.com>
2023-02-27 13:26:34 -0600

    phase 3

--- Program.cs
+++ Program.cs
@@ -7,15 +7,26 @@ namespace guessingGame
         static void Main(string[] args)
         {
             int secretNumber = 42;
+
+        for(int i = 1; i <= 4; i++){
+            Console.Clear();
             Console.WriteLine("Guess the secret number...");
+            Console.Write($"Attempt {i}/4: ");
             string input = Console.ReadLine();
             int parsedInput = int.Parse(input);
             if (parsedInput == secretNumber)
             {
                 Console.WriteLine("You guessed right, the secret number is 42!");
+                break;
             }
-            else 
-            Console.WriteLine($"{parsedInput} is not the secret number, try again!");
+            if(i != 4 && parsedInput != secretNumber)
+                Console.WriteLine($"{parsedInput} is not the secret number, try again!");
+
+            if(i == 4 && parsedInput != secretNumber)
+            {
+                Console.WriteLine("Better luck next time!");
+            }
+        }
         }
     }
 }
