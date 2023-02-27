4fa55803e73eba2e1ac8f3f210db2deb5a829c86
Jeremy Goodman <jeremygoodman52@gmail.com>
2023-02-27 14:46:24 -0600

    phase7

--- Program.cs
+++ Program.cs
@@ -4,39 +4,66 @@ namespace guessingGame
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.Clear();
-            Random rnd = new Random();
-            int secretNumber = rnd.Next(1,100);
-
-        for(int i = 1; i <= 4; i++){
-            if(i == 1){
-            Console.WriteLine("Guess the secret number...");
-            }
-            Console.Write($"Attempt {i}/4: ");
-            string input = Console.ReadLine();
-            Console.Clear();
-            int parsedInput = int.Parse(input);
-            if (parsedInput == secretNumber)
+            Console.WriteLine("~~ Guessing Game!~~");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Select a difficulty");
+            Console.WriteLine("1 for Easy (8 guesses)");
+            Console.WriteLine("2 for Easy (6 guesses)");
+            Console.WriteLine("3 for Easy (4 guesses)");
+            int difficulty = int.Parse(Console.ReadLine());
+             int guesses = 0;
+            if (difficulty == 1)
             {
-                Console.WriteLine($"The correct answer is {secretNumber}!");
-                Console.WriteLine("Conglaturations, a winner is you!");
-                break;
+                guesses = 8;
             }
-            if(i != 4 && parsedInput != secretNumber)
-                if(secretNumber < parsedInput){
-                    Console.WriteLine($"{parsedInput} is to high! guess lower");
+            if (difficulty == 2)
+            {
+                guesses = 6;
+            }
+            if (difficulty == 3)
+            {
+                guesses = 4;
+            }
+        
+            Console.Clear();
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1, 100);
+
+            for (int i = 1; i <= guesses; i++)
+            {
+                if (i == 1)
+                {
+                    Console.WriteLine("Guess the secret number...");
                 }
-                if(secretNumber > parsedInput){
+                Console.Write($"Attempt {i}/{guesses}: ");
+                string input = Console.ReadLine();
+                Console.Clear();
+                int parsedInput = int.Parse(input);
+                if (parsedInput == secretNumber)
+                {
+                    Console.WriteLine($"The correct answer is {secretNumber}!");
+                    Console.WriteLine("Conglaturations, a winner is you!");
+                    break;
+                }
+                if (i != guesses && parsedInput != secretNumber)
+                    if (secretNumber < parsedInput)
+                    {
+                        Console.WriteLine($"{parsedInput} is to high! guess lower");
+                    }
+                if (secretNumber > parsedInput)
+                {
                     Console.WriteLine($"{parsedInput} is to low! guess higher");
                 }
 
-            if(i == 4 && parsedInput != secretNumber)
-            {
-                Console.WriteLine($"You lose. The number was {secretNumber}. Better luck next time!");
+                if (i == guesses && parsedInput != secretNumber)
+                {
+                    Console.WriteLine($"You lose. The number was {secretNumber}. Better luck next time!");
+                }
             }
-        }
         }
     }
 }
